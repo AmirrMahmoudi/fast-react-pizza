74a4f9a98e38b4d7a9023e7195f761c754c02ea1
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-05 15:51:54 +0330

    Implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/manu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
-export default function App() {
-  return <div>Hello Vite!</div>;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
