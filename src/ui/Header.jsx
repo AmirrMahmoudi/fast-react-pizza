85421c8bdf70a9fc72915da80f290e13f8023ca1
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-07 16:44:40 +0330

    Styling Buttons Element States and Transitions

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 const Header = () => {
   return (
-    <header className="sm:px flex items-center justify-around border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="sm:px flex items-center justify-around border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase">
       <Link to={'/'} className="tracking-widest">
         Fast React Pizza Co.
       </Link>
