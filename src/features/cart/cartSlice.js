5bce350103b8385e3150f4314da814648caf45c6
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-09 12:34:07 +0330

    Updating cart quantities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -29,7 +29,7 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity++;
-      item.totalPrice = item.quantity * item.untiPrice;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
       // payload= pizzaId
@@ -37,7 +37,9 @@ const cartSlice = createSlice({
 
       item.quantity--;
       item.totalPrice = item.quantity * item.untiPrice;
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
+
     clearCart(state) {
       state.cart = [];
     },
@@ -64,7 +66,5 @@ export const getTotalCartPrice = (state) =>
 
 export const getCurrentQuantityById = (id) => (state) =>
   state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
-
-  
 
 //   reselect
--- src/features/manu/MenuItem.jsx
+++ src/features/manu/MenuItem.jsx
@@ -4,14 +4,13 @@ import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
 import { formatCurrency } from '../../utils/helpers';
 import Button from '../../ui/Button';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   const currentQuantity = useSelector(getCurrentQuantityById(id));
-  console.log(currentQuantity);
-
   const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
@@ -46,7 +45,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button onClick={handleAddToCart} type="small">
